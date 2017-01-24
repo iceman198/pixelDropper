280a59e365e9503778af62a67d348ad8ca8f772c
iceman198 <iceman198@gmail.com>
2017-01-24 13:28:56 -0500

    Added the rest

---
+++ pulse.js
@@ -0,0 +1,146 @@
+var async = require("async");
+
+var tinycolor = require('./tinycolor');
+var logger = require('./logger.js');
+
+var OPC = new require('./opc')
+//var client = new OPC('localhost', 7890);
+var client = new OPC('10.9.17.63', 7890);
+
+var filledPixArr = [];
+
+var stripStart = 0; // you could tweak this if you only have a specific FC chip hooked up
+var strips = 31; // ~~ same as above ~~
+
+var ledStart = 0;
+var leds = 32;
+
+var speedLow = 100;
+var speedMax = 100;
+
+var colorPulse = tinycolor({r: 117, g: 245, b:117});
+
+exports.setPixel = function(pixelNum, color) {
+	colorPulse = getColor(color);
+	var red = colorPulse.toRgb().r;
+	var green = colorPulse.toRgb().g;
+	var blue = colorPulse.toRgb().b;
+	
+	client.setPixel(pixelNum, red, green, blue);
+}
+
+exports.sendPulse = function (stripNum, color) {	
+	colorPulse = getColor(color);
+
+	logger.log('debug', 'pulse.js', '** stripNum: ' + stripNum + ' // color: ' + color);
+
+	var speed = speedMax;
+	
+	var red = 0;
+	var green = 0;
+	var blue = 0;
+	var curLed = ledStart;
+	var led = ledStart;
+	async.whilst(
+		function () { return led < leds; }, // function will run while this is true
+		function (callback) {
+			// working here
+			for (strip = stripStart; strip <= strips; strip++) {
+				curLed = led+(strip*leds);
+				//logger.log('debug', 'pulse.js', 'On strip: ' + strip + ' led:' + led + ' actualLED: ' + curLed + ' stripNum: ' + stripNum);
+				
+				if (strip == stripNum) {
+					if (filledPixArr.indexOf(curLed) > -1) {
+						filledPixArr.push(curLed-1);
+						break;	
+					}
+					if (led == leds -1) {
+						filledPixArr.push(curLed);
+					}
+
+					red = colorPulse.toRgb().r;
+					green = colorPulse.toRgb().g;
+					blue = colorPulse.toRgb().b;
+					//logger.log('debug', 'pulse.js', 'setting color on strip: ' + strip + ' led:' + led + ' actualLED: ' + curLed + ' stripNum: ' + stripNum);
+					if (led > 0) {
+						client.setPixel(curLed, red, green, blue);
+						client.setPixel(curLed - 1, 0, 0, 0);
+						logger.log('debug', 'pulse.js', 'Just set led ' + curLed + ' on and led ' + (curLed - 1) + ' off');
+					} else {
+						client.setPixel(curLed, red, green, blue);
+						logger.log('debug', 'pulse.js', 'Just set led ' + curLed + ' on');
+					}
+				}
+				red = 0;
+				green = 0;
+				blue = 0;
+			}
+			led++;
+			setTimeout(function () {
+				callback(null, null);
+			}, speed);
+			//logger.log('debug', 'pulse.js', 'Finally - strip: ' + strip + ' led:' + led + ' actualLED: ' + curLed + ' stripNum: ' + stripNum);
+		},
+		function (err, n) {
+		}
+	);
+}
+
+exports.startup = function () {
+	async.whilst(
+		function () { return true; }, // function will run while this is true
+		function (callback) {
+			client.writePixels();
+			//logger.log('debug', 'pulse.js', 'Wrote pixels');
+			setTimeout(function () {
+				callback(null, null);
+			}, 10);
+		},
+		function (err, n) {
+			// 5 seconds have passed, n = 5
+			//logger.log('info', 'pulse.js', count);
+		}
+	);
+}
+
+exports.cleanSlate = function() {
+	for (led = ledStart; led < (strips+1)*leds; led++) {
+		//console.log('Cleaning led: ' + led);
+		client.setPixel(led, 0, 0, 0);
+	}
+	//console.log('Cleaning led: ' + led);
+	client.setPixel(led, 0, 0, 0);
+	client.writePixels();
+}
+
+exports.updatecounts = function(countDog, countCat, countOther) {
+	rowCountDog = parseInt(countDog) / divisibleBy;
+	rowCountCat = parseInt(countCat) / divisibleBy;
+	rowCountOther = parseInt(countOther) / divisibleBy;
+}
+
+exports.getLeds = function() {
+	return leds;
+}
+
+function getRandomInt(min, max) {
+	return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+function getColor(color) {
+	if (color.indexOf(':') > -1) {
+		var colorArr = color.split(':');
+		return tinycolor({r: colorArr[0], g: colorArr[1], b: colorArr[2]});
+	} else {
+		return tinycolor('#' + color);
+	}
+}
+
+function sleep(milliseconds) {
+	var tempTime = new Date().getTime();
+	for (var i = 0; i < 1e7; i++) {
+		if ((new Date().getTime() - tempTime) > milliseconds){
+			break;
+		}
+	}
+}
---
+++ serverChild.js
@@ -0,0 +1,128 @@
+/**
+	author: Sean Dutton @seantdutton
+**/
+
+var express = require('express');
+var app = express();
+var logger = require('./logger');
+
+var picFile = 'unicorn.json';
+
+var port = 8080;
+
+var pictime = 50;
+
+// this lets me take an argument that is passed when the script is started
+//     example: $node serverChild.js 8081 << 8081 will be the port used in this case
+process.argv.forEach(function (val, index, array) {
+  //console.log(index + ': ' + val);
+  //port = val;
+});
+
+app.get('/pic', function(req, res) {
+	res.send('SUCCESS');
+	
+	var fs = require('fs');
+	var obj = JSON.parse(fs.readFileSync(picFile, 'utf8'));
+	for (i = 0; i < obj.length; i++) { 
+		var pixel = obj[i].ledNum;
+		var color = obj[i].r + ':' + obj[i].g + ':' + obj[i].b;
+		
+			mypassthrough(pixel, color, (i*pictime));
+	}
+});
+
+function mypassthrough(pixel, color, time) {
+	setTimeout(function() {
+		var pulse = require('./pulse');
+		pulse.setPixel(pixel, color);
+		logger.log('debug', 'serverChild.js', 'mypassthrough() ~ setting pixel ' + pixel + ' to color ' + color);
+	}, time);
+}
+
+app.get('/pixel/:pixelNum/:color', function(req, res) {
+	var pulse = require('./pulse');
+	var pixel = req.params.pixelNum;
+	var color = req.params.color;
+
+	logger.log('debug', 'serverChild.js', 'I see an pixel # of: ' + pixel + ' and color: ' + color);
+	var statusStr = {
+			status: "SUCCESS",
+			pixel: pixel, 
+			color: color
+		};
+	res.send(JSON.stringify(statusStr));
+	pulse.setPixel(pixel, color);
+});
+
+app.get('/dropper/:strip/:color', function(req, res) {
+	var pulse = require('./pulse');
+	
+	var strip = req.params.strip;
+	var color = req.params.color;
+	
+	logger.log('debug', 'serverChild.js', 'I see an strip # of: ' + strip + ' and color: ' + color);
+	var statusStr = {
+			status: "SUCCESS",
+			strip: strip, 
+			color: color
+		};
+	res.send(JSON.stringify(statusStr));
+	
+	pulse.sendPulse(strip, color);
+});
+
+app.get('/service/:cmd', function(req, res) {
+	var cmd = req.params.cmd;
+	if (cmd == 'resetcounts') {
+		var pulse = require('./pulse');
+		pulse.resetcounts();
+		res.end('SUCCESS');
+		logger.log('info', 'serverChild.js', 'counts reset');
+	} else if (cmd.indexOf('updatecounts') != -1) {
+		var pulse = require('./pulse');
+		var temp = cmd.replace('updatecounts,', '');
+		var arr = temp.split(',');
+		
+		if (arr.length == 3) {
+			pulse.updatecounts(arr[0], arr[1], arr[2]);
+			res.end('SUCCESS');
+			logger.log('info', 'serverChild.js', 'counts updated: ' + arr);
+		} else {
+			res.end('ERROR: array length of ' + arr.length + ' is incorrect (' + arr + ') ' + '(' + cmd + ') ' + '(' + temp + ')');
+		}
+	} else if (cmd == 'reboot') {
+		console.log('reboot issued');
+		var exec  = require("child_process").exec ;
+		exec ('reboot', function(error, stdout, stderr) {
+			// nothing for now
+		});
+	} else if (cmd == 'shutdown') {
+		console.log('shutdown issued');
+		var exec  = require("child_process").exec ;
+		exec ('shutdown now', function(error, stdout, stderr) {
+			
+		});
+	} else {
+		res.end('Command ' + cmd + ' not recognized');
+	}
+});
+
+var server = app.listen(port, function() {
+	var pulse2 = require('./pulse');
+	logger.log('info', 'serverChild.js', 'listening on http://localhost:' + port);
+	
+	pulse2.cleanSlate();
+	pulse2.cleanSlate(); // have to do it a second time for some reason so it doesn't sparkle..
+	pulse2.startup();
+});
+
+function sleep(milliseconds) {
+	var tempTime = new Date().getTime();
+	for (var i = 0; i < 1e7; i++) {
+		if ((new Date().getTime() - tempTime) > milliseconds){
+			break;
+		}
+	}
+}
+
