f322f32ecdb934b734c8b06e8d91477450400d86
iceman198 <iceman198@gmail.com>
2017-01-27 12:03:14 -0500

    b

--- pulse.js
+++ pulse.js
@@ -15,8 +15,8 @@ var strips = 31; // ~~ same as above ~~
 var ledStart = 0;
 var leds = 32;
 
-var speedLow = 100;
-var speedMax = 100;
+var speedLow = 50;
+var speedMax = 50;
 
 var colorPulse = tinycolor({r: 117, g: 245, b:117});
 
